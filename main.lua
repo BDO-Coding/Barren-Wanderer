d72fea26c5a8896ac4b843b315640fae82d386f1
ReltonTolpo <ori.taylor@gmail.com>
2016-10-06 14:28:56 +0100

    Added basic player class
    Haven't added any code to it but I will soon so pls no-one else edit

--- main.lua
+++ main.lua
@@ -1,19 +1,23 @@
 require "scroll"
+require "player"
  
 function love.load()
 
     scroll.load()
+    player.load()
 
 end
  
 function love.update(dt)
 
     UPDATE_SCROLL()
+    UPDATE_PLAYER()
 
 end
  
 function love.draw()
 
     DRAW_SCROLL()
+    DRAW_PLAYER()
   
 end
---
+++ player.lua
@@ -0,0 +1,25 @@
+player={}
+
+function player.load()
+
+end
+
+function player.update()
+
+end
+
+function player.draw()
+
+end
+
+function UPDATE_PLAYER()
+
+	player.update()
+
+end
+
+function DRAW_PLAYER()
+
+	player.draw()
+
+end
