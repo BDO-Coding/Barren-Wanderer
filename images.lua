b9412fad3a5bc24be76574726e040f460058b2ba
ReltonTolpo <ori.taylor@gmail.com>
2016-10-07 18:31:51 +0100

    Made game work

--- images.lua
+++ images.lua
@@ -3,7 +3,6 @@ images={}
 function images.load()
 
 	images.playerDown = love.graphics.newImage("images/player/playerDown.png")
-	images.playerDownA = love.graphics.newImage("images/player/playerDownAnimation.png")
 	images.playerUp = love.graphics.newImage("images/player/playerUp.png")
 	images.playerSide = love.graphics.newImage("images/player/playerSide.png")
 
