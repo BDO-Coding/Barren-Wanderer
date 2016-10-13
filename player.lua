eb1600ee446896f1b20b525ed6373adc1ff5f11e
ReltonTolpo <ori.taylor@gmail.com>
2016-10-13 18:15:46 +0100

    Trying to make the player animated
    Please don't mess with this yet

--- player.lua
+++ player.lua
@@ -10,7 +10,8 @@ function player.load()
 	playerSizeY = 2
 	playerScreenX = 560
 	playerScreenY = 350
-	animeDelay = 2
+	animeDelay = 1000
+    aAlternate = true
 
 end
 
@@ -39,11 +40,46 @@ function player.draw()
         playerScreenX = 560
     end
 
-    if love.keyboard.isDown("a") then
-    	playerImage = images.playerSide
-    	playerSizeX = -2
-    	playerScreenX = 620
+    if love.keyboard.isDown("a") and aAlternate == true then
+        playerImage = images.playerDown
+        playerSizeX = -2
+        playerScreenX = 620
+        for i = 0, 1000, 1 do
+            animeDelay = animeDelay - 1
+            print(i, aAlternate)
+            if i < 1 then
+                aAlternate = false
+                animeDelay = 1000
+                print("hi")
+                break
+            end
+        end
+    elseif love.keyboard.isDown("a") and aAlternate == false then
+        playerImage = images.playerSide
+        playerSizeX = -2
+        playerScreenX = 620
+        for i = 0, 1000, 1 do
+            animeDelay = animeDelay - 1
+        end
+        if k == 0 then
+            aAlternate = true
+            animeDelay = 1000
+        end
     end
+
+    --[[function love.keypressed(aKey)
+        if aKey == "a" and aAlternate == true then
+            playerImage = images.playerSide
+            playerSizeX = -2
+            playerScreenX = 620
+            aAlternate = false
+        elseif aKey == "a" and aAlternate == false then
+            playerImage = images.playerDown
+            playerSizeX = -2
+            playerScreenX = 620
+            aAlternate = true
+        end
+    end]]
 
     if love.keyboard.isDown("d") then
     	playerImage = images.playerSide
