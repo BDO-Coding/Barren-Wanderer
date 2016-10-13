b203a50e9955db3f1b2a87c2bd36e4b1405e8651
ReltonTolpo <ori.taylor@gmail.com>
2016-10-13 21:07:36 +0100

    Done a hover over thing
    NOTE : BACKGROUND COLOUR OF MENU WILL CHANGE

--- menu.lua
+++ menu.lua
@@ -7,21 +7,43 @@ require "monster"
 function menu.load()
 
     images.load()
+    mouseX = love.mouse.getX()
+    mouseY = love.mouse.getY()
+
+end
+
+function menu.update(dt)
+
+    if inmenu == true then
+
+        mouseX = love.mouse.getX()
+        mouseY = love.mouse.getY()
+
+    end
 
 end
 
 function menu.draw()
 
     if inmenu == true then
-        love.graphics.setColor(255, 255, 255)
-        love.graphics.rectangle("fill", 170, 150, 220, 60)
+
+        love.graphics.setBackgroundColor( 17, 240, 240)
+
+        if mouseX > 170 and mouseX < 390 and mouseY > 150 and mouseY < 210 then
+            love.graphics.setColor(30, 125, 49)
+            love.graphics.rectangle("fill", 170, 150, 220, 60)
+        else
+            love.graphics.setColor(31, 191, 63)
+            love.graphics.rectangle("fill", 170, 150, 220, 60)
+        end
 
         love.graphics.setColor(0, 0, 0)
         love.graphics.print("Play", 255, 160, 0, 2, 3)
      
-        love.graphics.setColor(255, 255, 255)
+        love.graphics.setColor(0, 0, 0)
         love.graphics.printf("Barren World", -480, 60, 100, "center", 0, 3, 3, -155)
         love.graphics.printf("Created by Danny Harris and Ori Taylor", -380, 600, 1020, "center")
+
     end
 
 end
@@ -43,6 +65,7 @@ end
 function UPDATE_MENU(dt)
 
     love.mousepressed()
+    menu.update(dt)
 
 end
 
