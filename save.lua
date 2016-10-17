1510569eed5645d08e68b255e3f9952c5d47ee44
Danny Harris <danzo2303@gmail.com>
2016-10-17 20:13:16 +0100

    Prints seed on screen
    +Reading

--- save.lua
+++ save.lua
@@ -2,27 +2,14 @@ save={}
 require "player"
 require "scroll"
 
+seed = 0
+
 function save.load()
-
-save.openSeed(w)
-file:write("This is the webpage for writing to files:\nhttp://www.ac-web.org/forums/showthread.php?71015-Tutorial-Writing-to-a-File-With-Lua")
+save.clear()
+local file = io.open("world.txt", "a")
+file:write("Seed:\n1285\nhello\nmy\nname\nis\nDanny")
 file:close()
---save.clear()
-
-
-end
-
-function save.clear()
-
-file = io.open("world.txt", "w")
-file:close()
-
-end
-
-function save.openSeed(mode)
-
-file = io.open("seed.txt",mode)
-
+save.read()
 end
 
 function save.update(dt)
@@ -33,9 +20,44 @@ function save.draw()
 
 end
 
+function save.clear()
+
+local file = io.open("world.txt", "w")
+file:close()
+
+end
+
+
+function save.read()
+
+ local count = 1
+local file = io.open("world.txt", "r")
+seedNext = false
+io.input(file)
+
+    while true do
+      local line = io.read()
+      if line == nil then break end
+      count = count + 1
+    
+
+
+      if seedNext == true then
+      	seed = line
+      	seedNext = false
+      end
+
+      if line == "Seed:" then
+      seedNext = true
+      end
+      
+  end
+end
+
 function UPDATE_SAVE(dt)
 
 	save.update(dt)
+
 
 end
 
