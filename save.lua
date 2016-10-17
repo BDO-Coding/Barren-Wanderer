fc18ccd80eb6b7a2c8f27c352094450d0f272aa2
Danny Harris <danzo2303@gmail.com>
2016-10-17 19:15:39 +0100

    Stuff

--- save.lua
+++ save.lua
@@ -4,9 +4,24 @@ require "scroll"
 
 function save.load()
 
-file = io.open("world.txt", "w")
+save.openSeed(w)
 file:write("This is the webpage for writing to files:\nhttp://www.ac-web.org/forums/showthread.php?71015-Tutorial-Writing-to-a-File-With-Lua")
 file:close()
+--save.clear()
+
+
+end
+
+function save.clear()
+
+file = io.open("world.txt", "w")
+file:close()
+
+end
+
+function save.openSeed(mode)
+
+file = io.open("seed.txt",mode)
 
 end
 
