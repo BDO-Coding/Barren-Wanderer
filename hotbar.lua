e516d706901fea97e2a2380efc39c14bbcda0538
ReltonTolpo <ori.taylor@gmail.com>
2016-11-03 17:08:17 +0000

    Putting my changes back in

--- hotbar.lua
+++ hotbar.lua
@@ -7,18 +7,25 @@ function hotbar.load()
 	hotbarXCoord = 500
 	barLength = 4
 	barHeight = 2
+	leftX = 140
+	rightX = 373
+
 end
 
 function hotbar.update(dt)
 
 	healthLength = (health/maxHealth)*barLength
 	staminaLength = (stamina/maxStamina)*barLength
+	manaLength = (mana/maxMana)*barLength
+	hungerLength = (hunger/maxHunger)*barLength
 
 	if health <= 0 then healthLength = 0 end
 
 	if stamina <= 0 then staminaLength = 0 end
 
-	print(staminaLength)
+	if mana <= 0 then manaLength = 0 end
+
+	if hunger <= 0 then hungerLength = 0 end
 
 	function love.keypressed(numberKey)
 		if ingame == true and loadScreen == false then
@@ -53,7 +60,6 @@ function hotbar.draw()
 	hotbarXCoord = 500
 
 	if alive == true then
-
 		for i = 1, 10 do
 			if i == currentHotbarHand then
 				love.graphics.setColor(50, 50, 50)
@@ -68,16 +74,26 @@ function hotbar.draw()
 			end
 		end
 		love.graphics.setColor(255, 0, 0)
-		love.graphics.draw(images.guiBarInside, 100, 650, 0, healthLength, barHeight)
+		love.graphics.draw(images.guiBarInside, 70, 650, 0, healthLength, barHeight)
 		love.graphics.setColor(10, 255, 10)
-		love.graphics.draw(images.guiBarInside, 100, 680, 0, staminaLength, barHeight)
+		love.graphics.draw(images.guiBarInside, 70, 680, 0, staminaLength, barHeight)
+		love.graphics.setColor(147, 49, 245)
+		love.graphics.draw(images.guiBarInside, 300, 650, 0, manaLength, barHeight)
+		love.graphics.setColor(224, 157, 49)
+		love.graphics.draw(images.guiBarInside, 300, 680, 0, hungerLength, barHeight)
 		love.graphics.setColor(255, 255, 255)
-		love.graphics.draw(images.guiBar, 100, 650, 0, barLength, barHeight)
-		love.graphics.draw(images.guiBar, 100, 680, 0, barLength, barHeight)
-		love.graphics.draw(images.health, 60, 650, 0, barHeight, barHeight-(barHeight)*0.1)
-		love.graphics.draw(images.stamina, 60, 680, 0, barHeight, barHeight-(barHeight)*0.1)
-		love.graphics.print((math.floor(health)).."/"..maxHealth, 120, 660)
-		love.graphics.print((math.floor(stamina)).."/"..maxStamina, 120, 690)
+		love.graphics.draw(images.guiBar, 70, 650, 0, barLength, barHeight)
+		love.graphics.draw(images.guiBar, 70, 680, 0, barLength, barHeight)
+		love.graphics.draw(images.guiBar, 300, 650, 0, barLength, barHeight)
+		love.graphics.draw(images.guiBar, 300, 680, 0, barLength, barHeight)
+		love.graphics.draw(images.health, 33.6, 650, 0, barHeight, barHeight-(barHeight)*0.1)
+		love.graphics.draw(images.stamina, 33.6, 680, 0, barHeight, barHeight-(barHeight)*0.1)
+		love.graphics.draw(images.mana, 263.6, 650, 0, barHeight, barHeight-(barHeight)*0.1)
+		love.graphics.draw(images.hunger, 263.6, 680, 0, barHeight, barHeight-(barHeight)*0.1)
+		love.graphics.print((math.floor(health)).."/"..maxHealth, leftX, 660)
+		love.graphics.print((math.floor(stamina)).."/"..maxStamina, leftX, 690)
+		love.graphics.print((math.floor(mana)).."/"..maxMana, rightX, 660)
+		love.graphics.print((math.floor(hunger)).."/"..maxHunger, rightX, 690)
 	end
 
 end
--- player.lua
+++ player.lua
@@ -1,19 +1,17 @@
 player={}
-require "images"
+require"images"
 
 function player.load()
 
-	playerDefaultSpeed = 0.001
-	playerDefaultSpeedDiagonal = 0.0005
-    playerSprint = 3
-
-    playerSpeed = 1
-    playerSpeedDiagonal = 1
-
+	playerSpeed = 0.2 --0.002
+	playerSpeedDiagonal = 0.1 -- 0.001
+    playerSprint = 0.1
+    playerDefaultSpeed = 0.2
+    playerDefaultSpeedDiagonal = 0.1
 	doOnce = true
 	playerSizeX = 2
 	playerSizeY = 2
-	playerScreenX = 550
+	playerScreenX = 560
 	playerScreenY = 350
 
     animeDelayW = 0.1
@@ -25,7 +23,6 @@ function player.load()
     alternateA = true
     alternateS = true
     alternateD = true
-
     playerX = 1
     playerY = 1
     currentBiome = 1
@@ -34,17 +31,29 @@ function player.load()
     maxStamina = 100
     stamina = 100
     staminaRegen = 0.05
+
     maxHealth = 100
     health = 100
     healthRegen = 0.01
-    alive = true
 
+    maxMana = 50
+    mana = 50
+    manaRegen = 0.05
+
+    maxHunger = 100
+    hunger = 100
+    hungerRegen = 0.05
+
+    alive = true
 end
 
 function player.update(dt)
-
     if health < maxHealth then
         health = health + healthRegen
+    end
+
+    if mana < maxMana then
+        mana = mana + manaRegen
     end
 
     if love.keyboard.isDown("a")then
@@ -61,24 +70,17 @@ end
 
 function player.draw()
 
-    if health < 1 then
-        alive = false
-        love.graphics.setColor(255, 0, 0)
-        love.graphics.print("You died", 330, 300, 0, 10, 10)
-        love.graphics.setBackgroundColor(50, 50, 50)
-    end
-
 	if doOnce == true then
 		playerImage = images.playerDown
 		doOnce = false
 	end
 
-    if inmenu == false and alive == true then
+    if inmenu == false then
 
         if love.keyboard.isDown("w") and alternateW == true then
             playerImage = images.playerUpAnimeA
             playerSizeX = 2
-            playerScreenX = 550
+            playerScreenX = 560
             if animeDelayW <= 0 then
                 alternateW = false
                 animeDelayW = 0.4
@@ -86,7 +88,7 @@ function player.draw()
         elseif love.keyboard.isDown("w") and alternateW == false then
             playerImage = images.playerUpAnimeD
             playerSizeX = 2
-            playerScreenX = 550
+            playerScreenX = 560
             if animeDelayW <= 0 then
                 alternateW = true
                 animeDelayW = 0.4
@@ -96,7 +98,7 @@ function player.draw()
         if love.keyboard.isDown("s") and alternateS == true then
             playerImage = images.playerDownAnimeA
             playerSizeX = 2
-           playerScreenX = 550
+            playerScreenX = 560
             if animeDelayS <= 0 then
                 alternateS = false
                 animeDelayS = 0.4
@@ -104,7 +106,7 @@ function player.draw()
         elseif love.keyboard.isDown("s") and alternateS == false then
             playerImage = images.playerDownAnimeD
             playerSizeX = 2
-            playerScreenX = 550
+            playerScreenX = 560
             if animeDelayS <= 0 then
                 alternateS = true
                 animeDelayS = 0.4
@@ -132,7 +134,7 @@ function player.draw()
         if love.keyboard.isDown("d") and alternateD == true then
             playerImage = images.playerDownAnimeD
             playerSizeX = 2
-            playerScreenX = 550
+            playerScreenX = 560
             if animeDelayD <= 0 then
                 alternateD = false
                 animeDelayD = 0.4
@@ -140,7 +142,7 @@ function player.draw()
         elseif love.keyboard.isDown("d") and alternateD == false then
             playerImage = images.playerSide
             playerSizeX = 2
-            playerScreenX = 550
+            playerScreenX = 560
             if animeDelayD <= 0 then
                 alternateD = true
                 animeDelayD = 0.4
@@ -148,42 +150,44 @@ function player.draw()
         end
     end
 
-    if love.keyboard.isDown("lctrl") and stamina > 0 then
-    	playerSpeed = playerDefaultSpeed*playerSprint
-    	playerSpeedDiagonal = playerDefaultSpeedDiagonal*playerSprint
-        if currentTile == 7 then
-            playerSpeed = 0
-            playerSpeedDiagonal = 0
+    if love.keyboard.isDown("lctrl") and stamina > 1 then
+        if love.keyboard.isDown("w") or love.keyboard.isDown("a") or love.keyboard.isDown("s") or love.keyboard.isDown("d") then
+        	playerSpeed = playerDefaultSpeed*playerSprint
+        	playerSpeedDiagonal = playerDefaultSpeedDiagonal*playerSprint
+            if currentTile == 7 then
+                playerSpeed = 0
+                playerSpeedDiagonal = 0
+            end
+            stamina = stamina - 0.25
         end
-        stamina = stamina - 0.25
+    end
+
+    if love.keyboard.isDown("lctrl") then
+    	playerSpeed = 0.004
+    	playerSpeedDiagonal = 0.003
     else
-    	playerSpeed = playerDefaultSpeed
-    	playerSpeedDiagonal = playerDefaultSpeedDiagonal
-        if currentTile == 7 then
-            playerSpeed = 0
-            playerSpeedDiagonal = 0
-        end
-        if stamina <= maxStamina then stamina = stamina + staminaRegen end
+    	playerSpeed = 0.002
+    	playerSpeedDiagonal = 0.001
     end
 
     function love.keyreleased(releaseImage)
-        if releaseImage == "a" or releaseImage == "s" or releaseImage == "d" and inmenu == false and alive == true then
+        if releaseImage == "a" or releaseImage == "s" or releaseImage == "d" and inmenu == false then
             playerImage = images.playerDown
             playerSizeX = 2
-            playerScreenX = 550
-        elseif releaseImage == "w" and inmenu == false and alive == true then
+            playerScreenX = 560
+        elseif releaseImage == "w" and inmenu == false then
             playerImage = images.playerUp
             playerSizeX = 2
-            playerScreenX = 550
+            playerScreenX = 560
         elseif releaseImage == "escape" then
-        	inmenu = not inmenu
-            options = false
+        	if ingame == true then
+        		inmenu = not inmenu
+        	end
         end
     end
 
-    if alive == true then
-        love.graphics.draw(playerImage, playerScreenX, playerScreenY, 0, playerSizeX, playerSizeY)
-    end
+	love.graphics.draw(playerImage, playerScreenX, playerScreenY, 0, playerSizeX, playerSizeY)
+
 end
 
 function UPDATE_PLAYER(dt)
