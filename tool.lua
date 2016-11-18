10eea217ded57452048f0ba0a72ac089fc04d4a0
Danny Harris <danzo2303@gmail.com>
2016-11-18 07:42:53 +0000

    Tool array

--- tool.lua
+++ tool.lua
@@ -3,7 +3,7 @@ require "images"
 
 function tool.load ()
 toolIndex{{}}
-toolIndex[1] = {"Name","Type()"}
+toolIndex[1] = {"Name","Type(sword,dagger,bow,staff,pickaxe)",true--[[uses right hand]],false--[[uses left hand]]}
 end
 
 function tool.draw()
