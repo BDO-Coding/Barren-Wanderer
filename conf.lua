e04a803b9ddcb680dc325ae99284f815d1701a23
ReltonTolpo <ori.taylor@gmail.com>
2016-11-18 07:33:04 +0000

    A change!

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
 
-	t.title = "Dannny is better than us all!"
+	t.title = "Donald Trump is better than us all!"
 	t.window.width = 1200
 	t.window.height = 750
 	t.window.icon = "images/player/playerDown.png"
