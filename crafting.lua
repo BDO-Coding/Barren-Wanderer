e73867bda545d1d82b40869dda17d6db166269c1
Danny Harris <danzo2303@gmail.com>
2016-11-20 18:20:50 +0000

    Fully working customisable recipe system!!

--- crafting.lua
+++ crafting.lua
@@ -4,10 +4,18 @@ require "images"
 function crafting.load ()
 
 	recipeIndex = {{}}
-	recipeIndex[1] = {true--[[if its known]],false--[[if selected]],1--[[itemID (result)]],1--[[crafterID]],{{1,1,1},{1,1,1},{1,1,1}}}
-	recipeIndex[2] = {true,false,2,1,{{1,1,1},{1,1,1},{1,1,1}}}
-	recipeIndex[3] = {true,false,3,1,{{1,1,1},{1,1,1},{1,1,1}}}
-	recipeIndex[4] = {true,false,4,1,{{1,1,1},{1,1,1},{1,1,1}}}
+	recipeIndex[1] = {true--[[if its known]],false--[[if selected]],1--[[itemID (result)]],{1,2,1,2}} -- ID,Amount ID,Amount
+	
+	recipeIndex[2] = {true,false,2,1,4} -- last num is amount of elements in ingredient array
+	recipeIndex[2][6] = {4,2,3,1}
+
+	recipeIndex[3] = {true,false,3,1,2}
+	recipeIndex[3][6] = {3,10}
+
+	recipeIndex[4] = {true,false,4,1,2}
+	recipeIndex[4][6] = {4,10}
+
+
 
 	inCrafting = false
 	wait = 0
@@ -21,6 +29,9 @@ function crafting.load ()
 	mouseX = 0
 	mouseY = 0
 
+	selectedRecipe = 2
+	unselected = true
+
 end
 
 function crafting.draw()
@@ -31,13 +42,19 @@ function crafting.draw()
 
 	if inCrafting == true then
 		love.graphics.draw(images.crafting,300,50,0,1.25,1.25)
+		love.graphics.print("Recipes:",397,110)
+		love.graphics.print("Crafting:",670,110)
+
+		if unselected == true then
+			love.graphics.print("Select a recipe by clicking on it.",620,140)
+		end
 
 		lineNum = 1
 
 		for i = 2, #recipeIndex do
 			if recipeIndex[i][1] == true then
 				if recipeIndex [i][2] == true then
-					love.graphics.setColor(50,50,50)
+					love.graphics.setColor(10,10,10)
 				else
 					love.graphics.setColor(255,255,255)
 				end
@@ -45,11 +62,33 @@ function crafting.draw()
 				lineNum = lineNum+1
 			end
 		end
+
+		if unselected == false then
+		ingredientNum = 1
+		lineNum = 1
+					love.graphics.setColor(255,255,255)
+		for i = 1, (recipeIndex[selectedRecipe][5])/2 do
+			ingredientNum = (i*2)-1
+			love.graphics.print(item.getItemName(recipeIndex[selectedRecipe][6][ingredientNum]).." x "..recipeIndex[selectedRecipe][6][ingredientNum+1],650,128+((lineNum-1)*lineSpacing))
+			lineNum = lineNum+1
+		end
+		end
+
 	end
 
 end
 
 function crafting.update(dt)
+
+	--Checks if nothing is selected:
+	unselected = true
+	for i = 2, #recipeIndex do
+		if recipeIndex[i][2] == true then
+			unselected = false
+		end
+	end
+
+
 
 	mouseX, mouseY = love.mouse.getX(),love.mouse.getY()
 
@@ -60,6 +99,8 @@ function crafting.update(dt)
 				if love.mouse.getX() > 360 and love.mouse.getX() < 500 then
 					if love.mouse.getY() > 128 + (lineSpacing*lineNum) and love.mouse.getY() < (128+lineSpacing) + (lineSpacing*lineNum) then
 						recipeIndex[i][2] = true
+						unselected = false
+						selectedRecipe = i
 					else
 						recipeIndex[i][2] = false
 					end
@@ -72,6 +113,12 @@ function crafting.update(dt)
 end
 
 	if keyPressed == true then
+		if inCrafting == false then
+			for i = 2,#recipeIndex do
+				recipeIndex[i][2] = false
+			    unselected = true
+			end
+		end
 		inCrafting = not inCrafting
 		keyPressed = false
 	end
